1a73aad204af5d23bbc893f8c98fa600ed2db142
Geoffroy Baghdassarian <geoffroy.baghdassarian@gmail.com>
2018-11-16 12:42:59 +0100

    initial commit

---
+++ bin/seed.js
@@ -0,0 +1,24 @@
+const plats = []
+
+
+REFERENCING DOCUMENTS
+
+
+
+Meat	Lamb	Lamb
+		Lamb chop
+		Rack ok lamb
+		Leg of lamb
+		Agneau aux herbes
+		Navarin of lamb
+		Shoulder of lamb
+		
+	Beef	Beef
+		Ribs
+		Roast beef
+		Steak
+		Flank steak
+		Skirt steak
+		Rib Steak
+		Beef stew
+		Steak tartare
