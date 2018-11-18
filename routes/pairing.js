5a67d0110e58cf2f3992e6aa9311658869bc8aa8
Geoffroy Baghdassarian <geoffroy.baghdassarian@gmail.com>
2018-11-18 16:42:34 +0100

    update routes

--- routes/pairing.js
+++ routes/pairing.js
@@ -12,80 +12,80 @@ router.get("/pairing", (req,res,next)=>{
 })
 
 
-// router.get("/meat", (req,res,next)=>{
+router.get("/meat", (req,res,next)=>{
   Meal.find({type : {$eq : "Meat"}})
   .then(data =>{
-    // res.locals.allMeat = data
+    res.locals.allMeat = data
     res.render("pairing/type-meat.hbs")
   })
   .catch(err=>next(err))
 })
 
-// router.get("/poultry", (req,res,next)=>{
+router.get("/poultry", (req,res,next)=>{
   Meal.find({type : {$eq : "Poultry"}})
   .then(data =>{
-    // res.locals.allMeat = data
+    res.locals.allMeat = data
     res.render("pairing/type-meat.hbs")
   })
   .catch(err=>next(err))
 })
 
-// router.get("/fish-and-seafood", (req,res,next)=>{
+router.get("/fish-and-seafood", (req,res,next)=>{
   Meal.find({type : {$eq : "Fish & Seafood"}})
   .then(data =>{
-    // res.locals.allMeat = data
+    res.locals.allMeat = data
     res.render("pairing/type-meat.hbs")
   })
   .catch(err=>next(err))
 })
 
 
-// router.get("/cheese", (req,res,next)=>{
+router.get("/cheese", (req,res,next)=>{
   Meal.find({type : {$eq : "Cheese"}})
   .then(data =>{
-    // res.locals.allMeat = data
+    res.locals.allMeat = data
     res.render("pairing/type-meat.hbs")
   })
   .catch(err=>next(err))
 })
 
-// router.get("/vegetables", (req,res,next)=>{
+router.get("/vegetables", (req,res,next)=>{
   Meal.find({type : {$eq : "Vegetable"}})
   .then(data =>{
-    // res.locals.allMeat = data
+    res.locals.allMeat = data
     res.render("pairing/type-meat.hbs")
   })
   .catch(err=>next(err))
 })
 
-// router.get("/other", (req,res,next)=>{
+router.get("/other", (req,res,next)=>{
   Meal.find({type : {$eq : "Others"}})
   .then(data =>{
-    // res.locals.allMeat = data
+    res.locals.allMeat = data
     res.render("pairing/type-meat.hbs")
   })
   .catch(err=>next(err))
 })
 
 
-// router.get("/:subtypeId", (req,res,next)=>{
+router.get("/:subtypeId", (req,res,next)=>{
   const subtypeId = req.params.subtypeId
   Meal.findById(subtypeId)
   .then(data =>{
-    // res.locals.subTypes = data
+    res.locals.subTypes = data
     res.render("pairing/subtypes.hbs")
   })
   .catch(err=>next(err))
 })
 
-// router.get("/wine-reco/:subtypeId", (req,res,next)=>{
+router.get("/wine-reco/:subtypeId", (req,res,next)=>{
   const subtypeId = req.params.subtypeId
   Meal.findById(subtypeId)
   .populate("wine")
   .then(data =>{
-    // res.locals.wineRecos = data
-    res.send(data.wine)
-    // res.render("pairing/wine-reco.hbs")
+    res.locals.wineRecos = data
+    // res.send(data.wine)
+    res.render("pairing/wine-reco.hbs")
   })
   .catch(err=>next(err))
 })
