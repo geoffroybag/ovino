eeb5991ab6ea573c042ca47d3583f81c14d5001a
Geoffroy Baghdassarian <geoffroy.baghdassarian@gmail.com>
2018-11-20 19:40:17 +0100

    google places

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -39,6 +39,47 @@ $(".fav-link").click(function () {
 
 
 
-// function with each 
-// = $(".section-reco").find('.selected').attr('name');
+  // Lie le champs adresse en champs autocomplete afin que l'API puisse afficher les propositions d'adresses
+  function initializeAutocomplete(id) {
+    var element = document.getElementById(id);
+    if (element) {
+     var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
+     google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+    }
+  }
+
+  var defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(48.856614, 2.3522219000000177),
+    new google.maps.LatLng(51.5073509, -0.12775829999998223));
+  
+  var input = document.getElementById('searchTextField');
+  var options = {
+    bounds: defaultBounds,
+  };
+  
+  autocomplete = new google.maps.places.Autocomplete(input, options);
+ 
+  // Injecte les données dans les champs du formulaire lorsqu'une adresse est sélectionnée
+  function onPlaceChanged() {
+    var place = this.getPlace();
+ 
+    for (var i in place.address_components) {
+      var component = place.address_components[i];
+      for (var j in component.types) {  
+        var type_element = document.getElementById(component.types[j]);
+        if (type_element) {
+          type_element.value = component.long_name;
+        }
+      }
+    }
+ 
+
+  }
+ 
+  // Initialisation du champs autocomplete
+  google.maps.event.addDomListener(window, 'load', function() {
+    initializeAutocomplete('user_input_autocomplete_address');
+  });
+
+ 
 
